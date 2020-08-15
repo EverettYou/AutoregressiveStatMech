4f3c6e3d11a0c43554710531f9ebad913a98e52c
Everett <everettyou.internet@gmail.com>
2020-08-15 13:51:27 -0700

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import torch
 import torch.optim as optim
 import pixel_gnn.model as model
 
+# set device
 parser = argparse.ArgumentParser()
 parser.add_argument('--disable-cuda', action='store_true', help='Disable CUDA')
 args = parser.parse_args()
@@ -11,7 +12,7 @@ if not args.disable_cuda and torch.cuda.is_available():
 else:
     model.device = torch.device('cpu')
 
-
+# setup model
 H = lambda J: -J*(model.TwoBody(torch.tensor([1.,-1.]), (1,0))
                 + model.TwoBody(torch.tensor([1.,-1.]), (0,1)))
 hpGNN = model.HolographicPixelGNN(
@@ -22,5 +23,24 @@ hpGNN = model.HolographicPixelGNN(
             hidden_features = [4, 4]).to(model.device)
 optimizer = optim.Adam(hpGNN.parameters(), lr=0.02)
 
-x = hpGNN.sample(3)
-print(x)
+# training
+batch_size = 100
+train_loss = 0.
+free_energy = 0.
+echo = 100
+for epoch in range(2000):
+    x = model.sample(batch_size)
+    log_prob = model.log_prob(x)
+    energy = model.energy(x)
+    free = energy + log_prob.detach()
+    meanfree = free.mean()
+    loss = torch.sum(log_prob * (free - meanfree))
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+    train_loss += loss.item()
+    free_energy += meanfree.item()
+    if (epoch+1)%echo == 0:
+        print('{:5} loss: {:8.4f}, free energy: {:8.4f}'.format(epoch+1, train_loss/echo, free_energy/echo))
+        train_loss = 0.
+        free_energy = 0.
