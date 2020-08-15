acd0a45b01f3541a27d7ace5f7cd2e90790e823c
Everett <everettyou.internet@gmail.com>
2020-08-15 14:12:08 -0700

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ hpGNN = model.HolographicPixelGNN(
                 H(0.440686793), # Ising critical point
                 model.SymmetricGroup(2), 
                 model.Lattice(4, 2)), 
-            hidden_features = [4, 4]).to(model.device)
+            hidden_features = [32, 32]).to(model.device)
 optimizer = optim.Adam(hpGNN.parameters(), lr=0.02)
 
 # training
